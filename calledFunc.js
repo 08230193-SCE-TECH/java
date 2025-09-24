909912dc66e11d105523184a43ce871cd2390550
unknown <08230193.sce@rub.edu.bt>
2025-09-23 21:23:08 -0700

    adding other files

---
+++ calledFunc.js
@@ -0,0 +1,14 @@
+function showdata(name, amt){
+    console.log('Hello' + name + '\n You are saving' + amt);
+
+
+
+}
+
+function getdata(callbring){
+    const prompt = require('prompt-sync')();
+    var name = prompt('Enter your name: ');
+    var amt = prompt('Enter the amount you want to save: ');
+    callbring(name, amt);
+}
+getdata(showdata);
